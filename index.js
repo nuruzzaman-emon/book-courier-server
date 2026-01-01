8321b29f6f3fc3aa1d059c1eae96536615f5aa0d
unknown <emon21108@gmail.com>
2026-01-01 13:33:37 +0600

    added get api for book details

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const admin = require("firebase-admin");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.Port || 3000;
+const serviceAccount = require("./book-courier-server-firebase-key.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 //middlewar
 app.use(express.json());
-
 app.use(cors());
+
+//verify firebase token
+const verifyFBToken = async (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ message: "Unauthorized Access" });
+  }
+  const token = authorization.split(" ")[1];
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized Access" });
+  }
+  try {
+    const decoded = await admin.auth().verifyIdToken(token);
+    req.decoded_email = decoded.email;
+    next();
+  } catch (error) {
+    return res.status(401).send({ message: "Unauthorized Access" });
+  }
+};
 
 const uri = process.env.URI;
 
@@ -28,7 +51,29 @@ async function run() {
 
     const db = client.db("book-courier-db");
     const usersCollection = db.collection("users");
+    const booksCollection = db.collection("books");
 
+    //verify admin
+    const verifyAdmin = async (req, res, next) => {
+      const query = { email: req.decoded_email };
+      const user = await usersCollection.findOne(query);
+      if (!user || user?.role !== "admin") {
+        return res.status(403).send({ message: "Forbidden Access" });
+      }
+      next();
+    };
+    //verify librarian
+    const verifyLibrarian = async (req, res, next) => {
+      const query = { email: req.decoded_email };
+      const user = await usersCollection.findOne(query);
+      console.log(user);
+      if (!user || user?.role !== "librarian") {
+        return res.status(403).send({ message: "Forbidden Access" });
+      }
+      next();
+    };
+
+    //user related apis
     app.post("/users", async (req, res) => {
       const user = req.body;
       const email = user.email;
@@ -37,8 +82,47 @@ async function run() {
         return res.send({ message: "User Already Axist" });
       }
       user.createdAt = new Date();
-      user.role = "user"
+      user.role = "user";
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+    app.get("/users/:email/role", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const result = await usersCollection.findOne(query);
+      res.send({ role: result?.role || "user" });
+    });
+
+    //book related apis
+
+    app.get("/books", verifyFBToken, verifyLibrarian, async (req, res) => {
+      const { email, status } = req.query;
+      if (email !== req.decoded_email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      const query = {};
+      if (email) {
+        query.authorEmail = email;
+      }
+      if (status) {
+        query.status = status;
+      }
+      const result = await booksCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/books", verifyFBToken, async (req, res) => {
+      const book = req.body;
+      book.createdAt = new Date();
+      if (book.status === "published") {
+        book.publishedAt = new Date();
+      }
+      const result = await booksCollection.insertOne(book);
+      res.send(result);
+    });
+    app.get("/book-details/:id", async (req, res) => {
+      const query = { _id: new ObjectId(req.params.id) };
+      const result = await booksCollection.findOne(query);
       res.send(result);
     });
 
